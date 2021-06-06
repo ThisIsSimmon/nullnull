033259a41b0d964aefaa5a7694878a544675e9ed
Simmon <simmon@SimmonnoMac-mini.local>
2021-06-06 14:24:02 +0900

    :construction: Creating HTML structures

--- front-page.php
+++ front-page.php
@@ -1,3 +1,9 @@
-<?php
+<?php get_header(); ?>
 
-echo 9;
+
+
+
+
+
+
+<?php get_footer(); ?>
---
+++ header.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html <?php language_attributes(); ?>>
+<head>
+	<meta charset="<?php bloginfo( 'charset' ); ?>" />
+	<meta name="viewport" content="width=device-width, initial-scale=1" />
+	<?php wp_head(); ?>
+</head>
+
+<body <?php body_class(); ?> data-barba="wrapper">
+
+    <header id="header">
+
+    </header>
+
+
+    <main id="main" data-barba="container" data-barba-namespace="home">
