8aaac7419c2cc3b5e306f7d0cc0f31b062cd8cec
Simmon <simmon@SimmonnoMac-mini.local>
2021-06-12 18:13:23 +0900

    :construction: Creating Pagination

--- front-page.php
+++ front-page.php
@@ -22,4 +22,14 @@
 </section>
 
 
+<div class="pagination">
+    <a href="" class="prev page-numbers"></a>
+    <a href="" class="page-numbers">1</a>
+    <a href="" class="page-numbers">2</a>
+    <a href="" class="page-numbers">3</a>
+    <span class="page-numbers current">4</span>
+    <a href="" class="page-numbers">5</a>
+    <a href="" class="next page-numbers"></a>
+</div>
+
 <?php get_footer(); ?>
