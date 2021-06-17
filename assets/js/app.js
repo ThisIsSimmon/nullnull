33a5061ce8fbf429ef2001a23abd1a995c93628b
Simmon <simmon@SimmonnoMac-mini.local>
2021-06-17 14:14:54 +0900

    :lipstick: Add menu animation.

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,11 @@
-import { menu } from "./class-menu.js";
-
-new menu();
+MicroModal.init({
+	onShow: (modal) => {
+		document.getElementById("js-menu-open").classList.add("active");
+	},
+	onClose: (modal) => {
+		document.getElementById("js-menu-open").classList.remove("active");
+	},
+	disableScroll: true,
+	disableFocus: true,
+	awaitCloseAnimation: true,
+});
