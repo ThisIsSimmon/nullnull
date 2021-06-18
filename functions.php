128b140bced1fb58cad430d3dbf8457c1c8fb5bc
Simmon <simmon@SimmonnoMac-mini.local>
2021-06-18 18:00:52 +0900

    :wrench: Add constant.

---
+++ functions.php
@@ -0,0 +1,8 @@
+<?php
+
+if ( ! defined( 'ABSPATH' ) ) die;
+
+
+define( 'THEME_URI',  get_theme_file_uri() );
+define( 'THEME_IMAGE_URI',  THEME_URI . '/assets/img/dist' );
+define( 'THEME_PATH',  get_theme_file_path() );
