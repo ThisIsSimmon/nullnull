f6612a4eefe72d1e336c8e7257cbf1e78ccba75a
Simmon <simmon@SimmonnoMac-mini.local>
2021-06-19 17:43:59 +0900

    :bento: Add event listener.

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,11 +1,13 @@
-MicroModal.init({
-	onShow: (modal) => {
-		document.getElementById("js-menu-open").classList.add("active");
-	},
-	onClose: (modal) => {
-		document.getElementById("js-menu-open").classList.remove("active");
-	},
-	disableScroll: true,
-	disableFocus: true,
-	awaitCloseAnimation: true,
+document.addEventListener('DOMContentLoaded', function () {
+	MicroModal.init({
+		onShow: (modal) => {
+			document.getElementById('js-menu-open').classList.add('active');
+		},
+		onClose: (modal) => {
+			document.getElementById('js-menu-open').classList.remove('active');
+		},
+		disableScroll: true,
+		disableFocus: true,
+		awaitCloseAnimation: true,
+	});
 });
