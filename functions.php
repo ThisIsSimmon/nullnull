8cf4a23aa92f05f116c8632442c5a2ec42477d0c
Simmon <simmon@SimmonnoMac-mini.local>
2021-06-21 21:05:23 +0900

    :sparkles: Add function.

--- functions.php
+++ functions.php
@@ -1,12 +1,43 @@
 <?php
 
-if ( ! defined( 'ABSPATH' ) ) die;
+defined( 'ABSPATH' ) || exit;
 
 
-define( 'THEME_URI',  get_theme_file_uri() );
-define( 'THEME_IMAGE_URI',  THEME_URI . '/assets/img/dist' );
-define( 'THEME_PATH',  get_theme_file_path() );
+define( 'THEME_URI', get_theme_file_uri() );
+define( 'THEME_IMAGE_URI', THEME_URI . '/assets/img/dist' );
+define( 'THEME_PATH', get_theme_file_path() );
 
+
+require THEME_PATH . '/inc/class-init.php';
+new Init();
 
 require THEME_PATH . '/inc/class-wp-head.php';
 new WP_Head();
+
+
+// カテゴリーの階層維持
+add_filter(
+	'wp_terms_checklist_args',
+	function( $args, $post_id ) {
+
+		if ( ! isset( $args['checked_ontop'] ) ) {
+			$args['checked_ontop'] = false;
+		}
+		return $args;
+	},
+	10,
+	2
+);
+
+add_filter( 'wp_sitemaps_enabled', '__return_false' );
+
+add_action(
+	'after_setup_theme',
+	function() {
+
+		add_theme_support( 'title-tag' );
+		add_theme_support( 'post-thumbnails' );
+		add_theme_support( 'automatic-feed-links' );
+
+	}
+);
