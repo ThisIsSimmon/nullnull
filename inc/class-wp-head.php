10be858c51133e8aefd650655ad9e5fa16b13d0d
Simmon <simmon@SimmonnoMac-mini.local>
2021-06-21 21:08:56 +0900

    :recycle: Refactor way to load google fonts.

--- inc/class-wp-head.php
+++ inc/class-wp-head.php
@@ -2,36 +2,54 @@
 
 class WP_Head {
 
-    public function __construct() {
-        add_action( 'wp_head', [ $this, 'faster_tags' ], 1 );
-        add_action( 'wp_head', [ $this, 'enqueue_debug_mode_styles' ], 1 );
-        add_action( 'wp_enqueue_scripts', [ $this, 'enqueue_styles' ], 10 );
-        add_action( 'wp_enqueue_scripts', [ $this, 'enqueue_scripts' ], 11 );
+	public function __construct() {
+		add_action( 'wp_head', array( $this, 'faster_tags' ), 1 );
+		add_action( 'wp_enqueue_scripts', array( $this, 'enqueue_debug_mode_styles' ), 1 );
+		add_action( 'wp_enqueue_scripts', array( $this, 'enqueue_styles' ), 10 );
+		add_action( 'wp_enqueue_scripts', array( $this, 'enqueue_scripts' ), 11 );
 
-    }
+	}
 
-    public function enqueue_styles () {
-        wp_enqueue_style( 'google-fonts', 'https://fonts.googleapis.com/css2?family=M+PLUS+Rounded+1c:wght@400;500;700&family=Quicksand:wght@500;700&display=swap', [], null, 'all' );
-        if ( 'local' !== wp_get_environment_type() ) {
-            wp_enqueue_style( 'style', THEME_URI . '/style.css', ['google-fonts'], filemtime( THEME_PATH . '/style.css' ), 'all' );
-        }
-    }
+	public function enqueue_debug_mode_styles() {
+		if ( 'local' === wp_get_environment_type() ) {
+			wp_enqueue_style( 'style', THEME_URI . '/style.css', array( 'google-fonts' ), filemtime( THEME_PATH . '/style.css' ), 'all' );
+		}
+	}
 
-    public function enqueue_scripts() {
-        wp_enqueue_script( 'focus-visible', THEME_URI . '/assets/js/focus-visible.min.js', [], false, false );
-        wp_enqueue_script( 'micromodal', THEME_URI . '/assets/js/micromodal.min.js', [], false, false );
-        wp_enqueue_script( 'app', THEME_URI . '/assets/js/app.js', ['micromodal'], filemtime( THEME_PATH . '/assets/js/app.js' ), false );
-    }
+	public function enqueue_styles() {
+		// wp_enqueue_style( 'google-fonts', 'https://fonts.googleapis.com/css2?family=M+PLUS+Rounded+1c:wght@400;500;700&family=Quicksand:wght@500;700&display=swap', array(), false, 'all' );
+		wp_enqueue_style( 'google-fonts', $this->all_google_fonts(), array(), false, 'all' );
+		if ( 'local' !== wp_get_environment_type() ) {
+			wp_enqueue_style( 'style', THEME_URI . '/style.css', array( 'google-fonts' ), filemtime( THEME_PATH . '/style.css' ), 'all' );
+		}
+	}
+
+	public function enqueue_scripts() {
+		wp_enqueue_script( 'focus-visible', THEME_URI . '/assets/js/focus-visible.min.js', array(), '5.2.0', false );
+		wp_enqueue_script( 'micromodal', THEME_URI . '/assets/js/micromodal.min.js', array(), '0.4.6', false );
+		wp_enqueue_script( 'app', THEME_URI . '/assets/js/app.js', array( 'micromodal' ), filemtime( THEME_PATH . '/assets/js/app.js' ), false );
+	}
 
 
-    public function faster_tags() {
-        echo '<link rel="preconnect" href="https://fonts.gstatic.com">';
-    }
+	public function faster_tags() {
+		echo '<link rel="preconnect" href="https://fonts.gstatic.com">';
+	}
 
-    function enqueue_debug_mode_styles() {
-        if ( 'local' === wp_get_environment_type() ) {
-            echo '<link rel="stylesheet" href="' . THEME_URI . '/style.css">';
-        }
-    }
 
+	private function all_google_fonts() {
+		$fonts = array(
+			'M+PLUS+Rounded+1c:wght@400;500;700',
+			'Quicksand:wght@500;700',
+			'display=swap',
+		);
+
+		$fonts_collection = add_query_arg(
+			array(
+				'family' => implode( '&', $fonts ),
+			),
+			'https://fonts.googleapis.com/css2'
+		);
+
+		return $fonts_collection;
+	}
 }
