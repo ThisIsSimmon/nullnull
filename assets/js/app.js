16d798aaacc98afa3f2543f6ef1e946e8fa090dc
Simmon <simmon@SimmonnoMac-mini.local>
2021-06-23 10:40:53 +0900

    :fire: Remove event listener.

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,13 +1,11 @@
-document.addEventListener('DOMContentLoaded', function () {
-	MicroModal.init({
-		onShow: (modal) => {
-			document.getElementById('js-menu-open').classList.add('active');
-		},
-		onClose: (modal) => {
-			document.getElementById('js-menu-open').classList.remove('active');
-		},
-		disableScroll: true,
-		disableFocus: true,
-		awaitCloseAnimation: true,
-	});
+MicroModal.init({
+	onShow: (modal) => {
+		document.getElementById('js-menu-open').classList.add('active');
+	},
+	onClose: (modal) => {
+		document.getElementById('js-menu-open').classList.remove('active');
+	},
+	disableScroll: true,
+	disableFocus: true,
+	awaitCloseAnimation: true,
 });
