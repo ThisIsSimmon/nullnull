2b0e153504a687c6c27cd4022cb04c16afb51546
Simmon <simmon@SimmonnoMac-mini.local>
2021-06-23 11:11:14 +0900

    :art: Separate pagination into separate files.

--- front-page.php
+++ front-page.php
@@ -19,14 +19,6 @@
 </section>
 
 
-<div class="pagination">
-	<a href="" class="prev page-numbers"></a>
-	<a href="" class="page-numbers">1</a>
-	<a href="" class="page-numbers">2</a>
-	<a href="" class="page-numbers">3</a>
-	<span class="page-numbers current">4</span>
-	<a href="" class="page-numbers">5</a>
-	<a href="" class="next page-numbers"></a>
-</div>
+<?php get_template_part( 'template-parts/pagination' ); ?>
 
 <?php get_footer(); ?>
---
+++ template-parts/pagination.php
@@ -0,0 +1,9 @@
+<div class="pagination">
+	<a href="" class="prev page-numbers"></a>
+	<a href="" class="page-numbers">1</a>
+	<a href="" class="page-numbers">2</a>
+	<a href="" class="page-numbers">3</a>
+	<span class="page-numbers current">4</span>
+	<a href="" class="page-numbers">5</a>
+	<a href="" class="next page-numbers"></a>
+</div>
