3168e61b3da3e9e91e39492ba00ba6584c181d7a
Simmon <simmon@SimmonnoMac-mini.local>
2021-06-23 14:38:20 +0900

    :bento: Add GSAP library.

--- inc/class-wp-head.php
+++ inc/class-wp-head.php
@@ -26,7 +26,9 @@ class WP_Head {
 	public function enqueue_scripts() {
 		wp_enqueue_script( 'focus-visible', THEME_URI . '/assets/js/focus-visible.min.js', array(), '5.2.0', false );
 		wp_enqueue_script( 'micromodal', THEME_URI . '/assets/js/micromodal.min.js', array(), '0.4.6', false );
-		wp_enqueue_script( 'app', THEME_URI . '/assets/js/app.js', array( 'micromodal' ), filemtime( THEME_PATH . '/assets/js/app.js' ), false );
+		wp_enqueue_script( 'gsap', THEME_URI . '/assets/js/gsap.min.js', array(), '3.7.0', false );
+		wp_enqueue_script( 'ScrollTrigger', THEME_URI . '/assets/js/ScrollTrigger.min.js', array( 'gsap' ), '3.7.0', false );
+		wp_enqueue_script( 'app', THEME_URI . '/assets/js/app.js', array( 'micromodal', 'ScrollTrigger' ), filemtime( THEME_PATH . '/assets/js/app.js' ), false );
 	}
 
 
