d1f736a7d75f5d207ddd1e73fced6c86b94f6d92
Simmon <simmon@SimmonnoMac-mini.local>
2021-06-24 10:30:54 +0900

    :rotating_light: Disable svg auto minify.

--- imagemin.js
+++ imagemin.js
@@ -1,32 +1,29 @@
-const imagemin = require('imagemin-keep-folder')
-const imageminMozjpeg = require('imagemin-mozjpeg')
-const imageminPngquant = require('imagemin-pngquant')
-const imageminGifsicle = require('imagemin-gifsicle')
-const imageminSvgo = require('imagemin-svgo')
+const imagemin = require('imagemin-keep-folder');
+const imageminMozjpeg = require('imagemin-mozjpeg');
+const imageminPngquant = require('imagemin-pngquant');
+const imageminGifsicle = require('imagemin-gifsicle');
+// const imageminSvgo = require('imagemin-svgo')
 const imageminWebp = require('imagemin-webp');
 
+imagemin([process.argv[2]], {
+	plugins: [
+		imageminMozjpeg({ quality: 65 }),
+		imageminPngquant(),
+		imageminGifsicle(),
+		// imageminSvgo(),
+	],
+	replaceOutputDir: (output) => {
+		return output.replace(/\/src\//, '/dist/');
+	},
+}).then(() => {
+	console.log('The image has been compressed.');
+});
 
 imagemin([process.argv[2]], {
-    plugins: [
-        imageminMozjpeg({quality: 65}),
-        imageminPngquant(),
-        imageminGifsicle(),
-        imageminSvgo(),
-    ],
-    replaceOutputDir: output => {
-        return output.replace(/\/src\//, '/dist/')
-    },
+	plugins: [imageminWebp({ quality: 75 })],
+	replaceOutputDir: (output) => {
+		return output.replace(/\/src\//, '/dist/');
+	},
 }).then(() => {
-    console.log('The image has been compressed.')
-})
-
-imagemin([process.argv[2]], {
-    plugins: [
-        imageminWebp({quality: 75}),
-    ],
-    replaceOutputDir: output => {
-        return output.replace(/\/src\//, '/dist/')
-    },
-}).then(() => {
-    console.log('The webp has been generated.')
-})
+	console.log('The webp has been generated.');
+});
