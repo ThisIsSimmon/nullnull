93d0f2566d54a4333d67f454ea0d0689db9e532c
Simmon <simmon@SimmonnoMac-mini.local>
2021-06-29 17:39:04 +0900

    :construction: Add validation, send function.

--- assets/js/class-validate.js
+++ assets/js/class-validate.js
@@ -1,7 +1,7 @@
 export default class Validate {
 	constructor() {
 		Object.defineProperties(this, {
-			errorMessages: {
+			feedbacks: {
 				value: {
 					name: {
 						empty: 'お名前を入力してください',
@@ -12,6 +12,10 @@ export default class Validate {
 					},
 					message: {
 						empty: 'メッセージを入力してください',
+					},
+					submit: {
+						failure: '送信に失敗しました。時間を置いて再度お試しいただくか、Twitterからご連絡ください。',
+						success: '送信が完了しました。返信まで今しばらくお待ちくださいませ。',
 					},
 				},
 			},
@@ -37,18 +41,16 @@ export default class Validate {
 	validate = () => {
 		let isValid = true;
 		for (const [property, elements] of Object.entries(this.fields)) {
-			const errorElement = document.querySelector(`label[for="${property}"] > .contact-form__error`);
+			const errorElement = document.querySelector(`.contact-form__error--${property}`);
 			if ('' === elements.value) {
+				this.showFeedbacks({ [property]: 'empty' });
 				isValid = false;
-				errorElement.textContent = this.errorMessages[property].empty;
-				errorElement.setAttribute('aria-hidden', false);
 				continue;
 			}
 
 			if ('email' === property && !this.emailIsValid(elements.value)) {
 				isValid = false;
-				errorElement.textContent = this.errorMessages[property].invalid;
-				errorElement.setAttribute('aria-hidden', false);
+				this.showFeedbacks({ [property]: 'invalid' });
 				continue;
 			}
 
@@ -63,6 +65,36 @@ export default class Validate {
 	};
 
 	send = () => {
-		console.log('send!!!');
+		const contactForm = document.getElementById('js-contact-form');
+		const url = contactForm.getAttribute('action');
+		const formData = new FormData(contactForm);
+		formData.append('action', 'send_email');
+
+		fetch(url, {
+			method: 'POST',
+			credentials: 'same-origin',
+			body: formData,
+		})
+			.then((response) => {
+				if (!response.ok) {
+					console.log(response);
+					throw new Error(response.statusText);
+				}
+				return response.json();
+			})
+			.then((data) => {
+				this.showFeedbacks(data);
+			})
+			.catch((error) => {
+				this.showFeedbacks({ submit: 'failure' });
+			});
+	};
+
+	showFeedbacks = (errorObject) => {
+		for (const [property, status] of Object.entries(errorObject)) {
+			const errorElement = document.querySelector(`.contact-form__error--${property}`);
+			errorElement.textContent = this.feedbacks[property][status];
+			errorElement.setAttribute('aria-hidden', false);
+		}
 	};
 }
